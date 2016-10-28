36b09d74d194646493d9d96f69ae0909697be748
Tiago Oliveira <tiago.oliveira@gmx.us>
2016-10-27 22:24:19 -0200

    Saving on riak

--- apps/erlang_reactive_distributed/src/erlang_reactive_distributed_app.erl
+++ apps/erlang_reactive_distributed/src/erlang_reactive_distributed_app.erl
@@ -21,8 +21,12 @@ start(_StartType, _StartArgs) ->
       Value  -> {ApplicationPort, _} = string:to_integer(Value), ApplicationPort
   end,
 
+  {ok, RiakAddress} = application:get_env(riak_address),
+  {ok, RiakPort} = application:get_env(riak_port),
+  {ok, RiakPid} = riakc_pb_socket:start_link(RiakAddress, RiakPort),
+
   gen_event:start_link({global, event_bus}),
-  gen_event:add_handler({global, event_bus}, erlang_reactive_distributed_event, []),
+  gen_event:add_handler({global, event_bus}, erlang_reactive_distributed_event, RiakPid),
   leptus:start_listener(http, [{'_', [{erlang_reactive_distributed_handler, undef}]}], [{port, Port}]),
   erlang_reactive_distributed_sup:start_link().
 
--- apps/erlang_reactive_distributed/src/erlang_reactive_distributed_event.erl
+++ apps/erlang_reactive_distributed/src/erlang_reactive_distributed_event.erl
@@ -3,19 +3,13 @@
 
 -export([init/1, handle_event/2, handle_call/2, handle_info/2, code_change/3, terminate/2]).
 
-init(_Args) ->
-  {ok, RiakAddress} = application:get_env(riak_address),
-  {ok, Pid} = riakc_pb_socket:start_link(RiakAddress, 8087),
-  riakc_pb_socket:ping(Pid),
-  {ok, Pid}.
+init(RiakPid) ->
+  {ok, RiakPid}.
 
-handle_event(Event, Connection) ->
-  lager:info("New Event [~p] ~n", [Event]),
-
-  Event = riakc_obj:new(Event),
-  riakc_pb_socket:put(Connection, Event),
-
-  {ok, Connection}.
+handle_event(Event, RiakPid) ->
+  Object = riakc_obj:new(<<"index">>, <<"metadata">>, <<"data">>),
+  riakc_pb_socket:put(RiakPid, Object),
+  {ok, RiakPid}.
 
 handle_call(Call, State) ->
   lager:info("Handling call [~p] ~n", [Call]),
